9cc9c316231ac0b1c11531a18d0f128aefd7d960
getssh <gtamrat33@gmail.com>
2022-11-28 22:03:23 +0300

    data feched and rendered on screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState, useEffect, React } from 'react';
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState({});
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then(res => res.json())
+      .then(data => setAdvice(data.slip))
+  }, advice)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='advice-container'>
+        <p className='advice-id'>Advice #173</p>
+        <div className='advice'>
+          {advice && <p>{advice.advice}</p>}
+        </div>
+        <svg className='btn' width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
+      </div>
     </div>
   );
 }
